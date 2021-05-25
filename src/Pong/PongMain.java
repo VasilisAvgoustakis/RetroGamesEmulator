e30fe45b95b80906d1f635994a98e0ff0922c87b
Francesco <76430216+Francesco-87@users.noreply.github.com>
2021-05-25 21:02:18 +0200

    new

--- src/Pong/PongMain.java
+++ src/Pong/PongMain.java
@@ -1,10 +1,41 @@
 package Pong;
 
+import java.awt.*;
+
 /** The main class for the game Pong*/
 
-public class PongMain {
+public class PongMain extends Canvas implements Runnable {
+
+    private static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+    private Thread thread;
+    private boolean running = false;
+
+    public PongMain(){
+        new Window(WIDTH, HEIGHT, "Welcome to Pong", this);
+    }
+
+    public synchronized void start(){
+    thread = new Thread(this);
+    thread.start();
+    running= true;
+    }
+    public synchronized void stop(){
+       try{
+           thread.join();
+           running = false;
+           }
+       catch(Exception e){
+           e.printStackTrace();
+       }
+    }
+
+
+    public void run() {
+
+    }
 
     public static void main(String[] args){
-        System.out.println("hello");
-    }
+        new PongMain();
+
+           }
 }
